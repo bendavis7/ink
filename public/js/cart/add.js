3b203cfc3333bd1ffd53749352233bb20a2f38be
Joe <joemunene91@gmail.com>
2024-12-13 10:08:05 -0800

    h

--- public/js/cart/add.js
+++ public/js/cart/add.js
@@ -61,7 +61,7 @@ function showThis() {
     if(joe && (JSON.parse(joe).length) > 0){
         setTimeout(() => { window.location.assign('download') }, 300);
     } else { 
-        var shortCutFunction = 'success'; var msg = `Your cart is empty... <br> add bank logs to cart. <hr class="to-hr hr15-bot">`; 
+        var shortCutFunction = 'success'; var msg = `Your cart is empty... <br> add some logs to cart. <hr class="to-hr hr15-bot">`; 
         toastr.options =  {closeButton: true, debug: false, newestOnTop: true, progressBar: true,positionClass: 'toast-top-full-width', preventDuplicates: true, onclick: null}; var $toast = toastr[shortCutFunction](msg);$toastlast = $toast; $('#profileModal').modal('hide'); 
     }
 }
