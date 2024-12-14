123adcfaac424c6f567bbb86185a7de357bbcfa9
Joe <joemunene91@gmail.com>
2024-12-14 13:53:12 -0800

    h

--- public/js/auth/download.js
+++ public/js/auth/download.js
@@ -146,27 +146,27 @@ auth.onAuthStateChanged(user => {
 							${toastbtci} BTC not detected, <br> ${user.email}        <hr class="to-hr hr15-top"> 
 							Verify your email inbox,  <br> Check the spam - folder.  <hr class="to-hr hr15-top"> 
 						`;
-						toastr.options =  {closeButton: true, debug: false, newestOnTop: true, progressBar: true, timeOut: 6000, positionClass: 'toast-top-full-width', preventDuplicates: true, onclick: null}; var $toast = toastr[shortCutFunction](msg);$toastlast = $toast;
+						toastr.options =  {closeButton: true, debug: false, newestOnTop: true, progressBar: true, timeOut: 7000, positionClass: 'toast-top-full-width', preventDuplicates: true, onclick: null}; var $toast = toastr[shortCutFunction](msg);$toastlast = $toast;
 					} else { 
 						var shortCutFunction = 'success';  
 						var msg = `
 							${toastbtci} BTC not detected, <br> Send exactly $${toastzi}. <hr class="to-hr hr15-top"> 
 							Bank logs will be sent to <br> ${user.email}.                 <hr class="to-hr hr15-top">
 						`;
-						toastr.options =  {closeButton: true, debug: false, newestOnTop: true, progressBar: true, timeOut: 6000, positionClass: 'toast-top-full-width', preventDuplicates: true, onclick: null}; var $toast = toastr[shortCutFunction](msg);$toastlast = $toast;
+						toastr.options =  {closeButton: true, debug: false, newestOnTop: true, progressBar: true, timeOut: 7000, positionClass: 'toast-top-full-width', preventDuplicates: true, onclick: null}; var $toast = toastr[shortCutFunction](msg);$toastlast = $toast;
 		
-						setTimeout(() => { generatePDF(); }, 8500);
+						setTimeout(() => { generatePDF(); }, 10000);
 					}
 				});
 			} else {
 				var shortCutFunction = 'success';
 				var msg = `
 					${toastbtci} BTC not detected, <br> Send exactly $${toastzi}. <hr class="to-hr hr15-top"> 
-					Logs can be saved as .PDF <br> file or sent via Email...      <hr class="to-hr hr15-top"> 
+					Bank logs can be sent as  <br> .PDF file or via Email ..      <hr class="to-hr hr15-top"> 
 				`;
-				toastr.options =  {closeButton: true, debug: false, newestOnTop: true, progressBar: true, timeOut: 6000, positionClass: 'toast-top-full-width', preventDuplicates: true, onclick: null}; var $toast = toastr[shortCutFunction](msg);$toastlast = $toast;
+				toastr.options =  {closeButton: true, debug: false, newestOnTop: true, progressBar: true, timeOut: 7000, positionClass: 'toast-top-full-width', preventDuplicates: true, onclick: null}; var $toast = toastr[shortCutFunction](msg);$toastlast = $toast;
 
-				setTimeout(() => { window.location.assign('home');}, 8500);
+				setTimeout(() => { window.location.assign('home');}, 10000);
 			}
 
 			var docRef = db.collection("users").doc(theGuy);
@@ -178,7 +178,7 @@ auth.onAuthStateChanged(user => {
 				}
 			});
 
-			setTimeout(() => { $('#exampleModal').modal('hide'); }, 4000);
+			setTimeout(() => { $('#exampleModal').modal('hide'); }, 5000);
 		});
 	}
 	document.getElementById('monez').addEventListener('click', signUpFunction);
