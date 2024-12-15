1de5804a5f6852ef71986138ede0590b07d70633
Joe <joemunene91@gmail.com>
2024-12-15 03:33:24 -0800

    Updates

--- js/item/download.js
+++ js/item/download.js
@@ -116,7 +116,7 @@ if(localStorage.getItem('banklogs')){
             setTimeout(() => { 
                 document.getElementById('modem').click(); 
                 localStorage.setItem('log-pdf', true) 
-            }, 11000);
+            }, 10000);
         }
 
         thetotS.addEventListener('click', ()=> { document.getElementById('modem').click(); });
@@ -125,12 +125,12 @@ if(localStorage.getItem('banklogs')){
         document.getElementById('the-logs').addEventListener('click', ()=> { document.getElementById('modem').click(); });
         document.getElementById('modem').addEventListener('click', () => { setTimeout(() => { monezB.click(); }, 2000); });
     } else {
-        document.getElementById('cartlength').style.display = 'none'; setTimeout(() => { window.location.assign('index'); }, 4000);
+        document.getElementById('cartlength').style.display = 'none'; setTimeout(() => { window.location.assign('index'); }, 5000);
         var shortCutFunction = 'success'; var msg = `Your cart is empty... <br> add some logs to cart. <hr class="to-hr hr15-bot">`; 
         toastr.options =  {closeButton: true, debug: false, newestOnTop: true, progressBar: true,positionClass: 'toast-top-full-width', preventDuplicates: true, onclick: null}; var $toast = toastr[shortCutFunction](msg);$toastlast = $toast; $('#profileModal').modal('hide'); 
     }
 } else {
-    document.getElementById('cartlength').style.display = 'none'; setTimeout(() => { window.location.assign('index'); }, 4000);
+    document.getElementById('cartlength').style.display = 'none'; setTimeout(() => { window.location.assign('index'); }, 5000);
     var shortCutFunction = 'success'; var msg = `Your cart is empty... <br> add some logs to cart. <hr class="to-hr hr15-bot">`; 
     toastr.options =  {closeButton: true, debug: false, newestOnTop: true, progressBar: true,positionClass: 'toast-top-full-width', preventDuplicates: true, onclick: null}; var $toast = toastr[shortCutFunction](msg);$toastlast = $toast; $('#profileModal').modal('hide'); 
 }
