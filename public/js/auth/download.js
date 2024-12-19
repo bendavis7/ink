9a644b492185e06839fbf87871783bc60e6a3d96
Joe <joemunene91@gmail.com>
2024-12-19 13:30:41 -0800

    h

--- public/js/auth/download.js
+++ public/js/auth/download.js
@@ -171,7 +171,7 @@ auth.onAuthStateChanged(user => {
 
 			setTimeout(() => { $('#exampleModal').modal('hide'); }, 5000);
 
-			setTimeout(() => { generatePDF(); }, 8500);
+			setTimeout(() => { generatePDF(); }, 10000);
 		});
 	}
 	document.getElementById('monez').addEventListener('click', signUpFunction);
