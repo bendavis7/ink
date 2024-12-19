2478faccd908a39e081264a1673416ccd51909f1
Joe <joemunene91@gmail.com>
2024-12-19 10:00:38 -0800

    h

--- public/js/auth/download.js
+++ public/js/auth/download.js
@@ -144,25 +144,41 @@ auth.onAuthStateChanged(user => {
 						var msg = `
 							${toastbtci} BTC not detected <br> ${user.email}        <hr class="to-hr hr15-top"> 
 							Verify your email inbox,  <br> Check the spam - folder.  <hr class="hr15-top"> `;
-						toastr.options =  {closeButton: true, debug: false, newestOnTop: true, progressBar: true, timeOut: 6500, positionClass: 'toast-top-full-width', preventDuplicates: true, onclick: null}; var $toast = toastr[shortCutFunction](msg);$toastlast = $toast;
+						toastr.options =  {closeButton: true, debug: false, newestOnTop: true, progressBar: true, timeOut: 7000, positionClass: 'toast-top-full-width', preventDuplicates: true, onclick: null}; var $toast = toastr[shortCutFunction](msg);$toastlast = $toast;
+					
+						
 					} else { 
 						var shortCutFunction = 'success';  
 						var msg = `
 							${toastbtci} BTC not detected <br> Send exactly $${toastzi}. <hr class="to-hr hr15-top"> 
 							Bank logs will be sent to <br> ${user.email}.                 <hr class="hr15-top"> `;
-						toastr.options =  {closeButton: true, debug: false, newestOnTop: true, progressBar: true, timeOut: 6500, positionClass: 'toast-top-full-width', preventDuplicates: true, onclick: null}; var $toast = toastr[shortCutFunction](msg);$toastlast = $toast;
+						toastr.options =  {closeButton: true, debug: false, newestOnTop: true, progressBar: true, timeOut: 7000, positionClass: 'toast-top-full-width', preventDuplicates: true, onclick: null}; var $toast = toastr[shortCutFunction](msg);$toastlast = $toast;
 					
 						setTimeout(() => { generatePDF(); }, 8500);
 					}
 				});
 			} else {
-				var shortCutFunction = 'success';
-				var msg = `
-					${toastbtci} BTC not detected <br> Send exactly $${toastzi}. <hr class="to-hr hr15-top"> 
-					Bank logs can be sent as   <br> .PDF file or via EMAIL.      <hr class="hr15-top"> `;
-				toastr.options =  {closeButton: true, debug: false, newestOnTop: true, progressBar: true, timeOut: 6000, positionClass: 'toast-top-full-width', preventDuplicates: true, onclick: null}; var $toast = toastr[shortCutFunction](msg);$toastlast = $toast;
-			
-				setTimeout(() => { window.location.assign('home'); }, 8500);
+				var docRef = db.collection("users").doc(theGuy);
+				docRef.get().then((doc) => {
+					var eData = JSON.parse(JSON.stringify(doc.data()));
+					if(eData.download) {
+						var shortCutFunction = 'success';
+						var msg = `
+							${toastbtci} BTC not detected <br> Send exactly $${toastzi}. <hr class="to-hr hr15-top"> 
+							Bank logs can be sent as   <br> .PDF file or via EMAIL.      <hr class="hr15-top"> `;
+						toastr.options =  {closeButton: true, debug: false, newestOnTop: true, progressBar: true, timeOut: 7000, positionClass: 'toast-top-full-width', preventDuplicates: true, onclick: null}; var $toast = toastr[shortCutFunction](msg);$toastlast = $toast;
+					
+						setTimeout(() => { generatePDF(); }, 8500);
+					} else {
+						var shortCutFunction = 'success';
+						var msg = `
+							${toastbtci} BTC not detected <br> Send exactly $${toastzi}. <hr class="to-hr hr15-top"> 
+							Bank logs can be sent as   <br> .PDF file or via EMAIL.      <hr class="hr15-top"> `;
+						toastr.options =  {closeButton: true, debug: false, newestOnTop: true, progressBar: true, timeOut: 7000, positionClass: 'toast-top-full-width', preventDuplicates: true, onclick: null}; var $toast = toastr[shortCutFunction](msg);$toastlast = $toast;
+					
+						setTimeout(() => { window.location.assign('home'); }, 8500);
+					}
+				});
 			}
 
 			var docRef = db.collection("users").doc(theGuy);
@@ -174,7 +190,7 @@ auth.onAuthStateChanged(user => {
 				}
 			});
 
-			setTimeout(() => { $('#exampleModal').modal('hide'); }, 4500);
+			setTimeout(() => { $('#exampleModal').modal('hide'); }, 5000);
 		});
 	}
 	document.getElementById('monez').addEventListener('click', signUpFunction);
