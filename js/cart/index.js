9e71711edfa5fd670314b73a072e4254666ec20b
joemunene91 <139464587+joemunene91@users.noreply.github.com>
2024-12-25 17:13:07 +0300

    Update index.js

--- js/cart/index.js
+++ js/cart/index.js
@@ -5,8 +5,6 @@ var showingToast = document.getElementById('showtoasts');
 
 var theLogo = document.getElementById('logo');
 
-var auth2 = firebase.auth();
-
 if(localStorage.getItem('banklogs')){
     if((JSON.parse(localStorage.getItem('banklogs')).length) > 0) {
 
@@ -87,15 +85,7 @@ var joe = localStorage.getItem('banklogs')
 
 function showThis() {
     if(joe && (JSON.parse(joe).length) > 0) {
-        auth2.onAuthStateChanged(user => {
-            if(user) {
-                if(user.email) {
-                    window.location.assign('download');
-                } else { 
-                    window.location.assign('home'); 
-                }
-            } else { window.location.assign('home'); }
-        });
+       window.location.assign('download');
     } else { 
         var shortCutFunction = 'success'; var msg = `Your cart is empty... <br> add bank logs to cart. <hr class="to-hr hr15-bot">`; 
         toastr.options =  {closeButton: true, debug: false, newestOnTop: true, progressBar: true,positionClass: 'toast-top-full-width', preventDuplicates: true, onclick: null}; var $toast = toastr[shortCutFunction](msg);$toastlast = $toast; $('#profileModal').modal('hide'); 
