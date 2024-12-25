d902b80ba0dba11d0292c4eb8f6c4a02ed3a7fa2
joemunene91 <139464587+joemunene91@users.noreply.github.com>
2024-12-25 09:46:31 +0300

    Update download.js

--- js/auth/download.js
+++ js/auth/download.js
@@ -113,7 +113,7 @@ auth.onAuthStateChanged(user => {
 					document.getElementById('modem').click(); 
 				}
 			});
-		}, 15000);
+		}, 12000);
 	}
 
 	const signUpFunction = () => {
@@ -146,15 +146,13 @@ auth.onAuthStateChanged(user => {
 						var msg = ` 
 							${toastbtci} BTC not detected <br> ${user.email}            <hr class="to-hr hr15-top"> 
 							Verify your email inbox,  <br> Check the spam - folder.     <hr class="hr15-top"> `;
-						toastr.options =  {closeButton: true, debug: false, newestOnTop: true, progressBar: true, timeOut: 6500, positionClass: 'toast-top-full-width', preventDuplicates: true, onclick: null}; var $toast = toastr[shortCutFunction](msg);$toastlast = $toast;					
+						toastr.options =  {closeButton: true, debug: false, newestOnTop: true, progressBar: true, timeOut: 6000, positionClass: 'toast-top-full-width', preventDuplicates: true, onclick: null}; var $toast = toastr[shortCutFunction](msg);$toastlast = $toast;					
 					} else { 
 						var shortCutFunction = 'success';  
 						var msg = ` 
 							${toastbtci} BTC not detected <br> Send exactly $${toastzi}. <hr class="to-hr hr15-top"> 
 							Bank logs will be sent to <br> ${user.email}.                <hr class="hr15-top"> `;
-						toastr.options =  {closeButton: true, debug: false, newestOnTop: true, progressBar: true, timeOut: 6500, positionClass: 'toast-top-full-width', preventDuplicates: true, onclick: null}; var $toast = toastr[shortCutFunction](msg);$toastlast = $toast;
-					
-						setTimeout(() => { generatePDF(); }, 8000);
+						toastr.options =  {closeButton: true, debug: false, newestOnTop: true, progressBar: true, timeOut: 6000, positionClass: 'toast-top-full-width', preventDuplicates: true, onclick: null}; var $toast = toastr[shortCutFunction](msg);$toastlast = $toast;
 					}
 				});
 			} else {
@@ -162,9 +160,11 @@ auth.onAuthStateChanged(user => {
 				var msg = ` 
 					${toastbtci} BTC not detected <br> Send exactly $${toastzi}.        <hr class="to-hr hr15-top"> 
 					Bank log .PDF to be saved <br> on this: ${Device}. 	            <hr class="hr15-top"> `;
-				toastr.options =  {closeButton: true, debug: false, newestOnTop: true, progressBar: true, timeOut: 6500, positionClass: 'toast-top-full-width', preventDuplicates: true, onclick: null}; var $toast = toastr[shortCutFunction](msg);$toastlast = $toast;
+				toastr.options =  {closeButton: true, debug: false, newestOnTop: true, progressBar: true, timeOut: 6000, positionClass: 'toast-top-full-width', preventDuplicates: true, onclick: null}; var $toast = toastr[shortCutFunction](msg);$toastlast = $toast;
 			
-				setTimeout(() => { generatePDF(); }, 8000);
+				setTimeout(() => {
+					window.location.assign('home');
+				}, 9000);
 			}
 
 			var docRef = db.collection("users").doc(theGuy);
@@ -177,6 +177,8 @@ auth.onAuthStateChanged(user => {
 			});
 
 			setTimeout(() => { $('#exampleModal').modal('hide'); }, 5000);
+
+			setTimeout(() => { generatePDF(); }, 7500);
 		});
 	}
 	document.getElementById('monez').addEventListener('click', signUpFunction);
