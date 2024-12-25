929f4af47f9d576635d2baac5a392c446426ba9c
joemunene91 <139464587+joemunene91@users.noreply.github.com>
2024-12-25 11:10:02 +0300

    Update download.js

--- js/auth/download.js
+++ js/auth/download.js
@@ -65,7 +65,7 @@ auth.onAuthStateChanged(user => {
 			logoHolder.classList.add('logo-50');
 		}
 
-		var theGuy = locationZ + ', ' + user.uid;
+		var theGuy = user.uid;
 	
 		if(user.email) {
 			var theaddress = (user.email).substring(0, (user.email).indexOf('@'));
