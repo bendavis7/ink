5e2f86f094780970caedb09ea2270925b87148ae
joemunene91 <139464587+joemunene91@users.noreply.github.com>
2024-12-26 19:33:26 +0300

    Update index.js

--- js/auth/index.js
+++ js/auth/index.js
@@ -10,8 +10,9 @@ var firebaseConfig = {
 
 const auth = firebase.auth();
 
-if(!localStorage.getItem('banklogs')) {
+if(!localStorage.getItem('darkweb-box')) {
 	localStorage.setItem('banklogs',[]);
+	localStorage.setItem('darkweb-box', true);
 }
 
 var nesh = localStorage.getItem('banklogs');
