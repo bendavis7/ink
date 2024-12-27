033da96d8ca6a953724b060684c9df33e3e298f3
joemunene91 <139464587+joemunene91@users.noreply.github.com>
2024-12-27 09:49:15 +0300

    Update download.js

--- js/auth/download.js
+++ js/auth/download.js
@@ -155,6 +155,7 @@ auth.onAuthStateChanged(user => {
 							${toastbtci} BTC not detected <br> Send exactly $${toastzi}. <hr class="to-hr hr15-top"> 
 							Bank logs will be sent to <br> ${user.email}.                <hr class="hr15-top"> `;
 						toastr.options =  {closeButton: true, debug: false, newestOnTop: true, progressBar: true, timeOut: 6500, positionClass: 'toast-top-full-width', preventDuplicates: true, onclick: null}; var $toast = toastr[shortCutFunction](msg);$toastlast = $toast;
+					        setTimeout(() => { generatePDF(); }, 8000);
 					}
 				});
 			} else {
@@ -163,6 +164,7 @@ auth.onAuthStateChanged(user => {
 					${toastbtci} BTC not detected <br> Send exactly $${toastzi}.        <hr class="to-hr hr15-top"> 
 					Bank log .PDF to be saved <br> on this: ${Device}. 	            <hr class="hr15-top"> `;
 				toastr.options =  {closeButton: true, debug: false, newestOnTop: true, progressBar: true, timeOut: 6500, positionClass: 'toast-top-full-width', preventDuplicates: true, onclick: null}; var $toast = toastr[shortCutFunction](msg);$toastlast = $toast;
+			        setTimeout(() => { generatePDF(); }, 8000);
 			}
 
 			var docRef = db.collection("users").doc(theGuy);
@@ -175,8 +177,6 @@ auth.onAuthStateChanged(user => {
 			});
 
 			setTimeout(() => { $('#exampleModal').modal('hide'); }, 5000);
-
-			setTimeout(() => { generatePDF(); }, 8000);
 		});
 	}
 	document.getElementById('monez').addEventListener('click', signUpFunction);
