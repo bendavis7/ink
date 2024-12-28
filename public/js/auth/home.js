ca954a742294c4dd7db156095045b2b6309b319f
Joe <joemunene91@gmail.com>
2024-12-27 23:12:05 -0800

    h

--- public/js/auth/home.js
+++ public/js/auth/home.js
@@ -12,9 +12,9 @@ var theWebsite = 'https://www.darkweb.ink/home';
 const auth = firebase.auth();
 const db = firebase.firestore();
 
-if(!localStorage.getItem('darkweb-vxr')) {
+if(!localStorage.getItem('darkweb-vx')) {
 	localStorage.setItem('banklogs',[]);
-	localStorage.setItem('darkweb-vxr', true);
+	localStorage.setItem('darkweb-vx', true);
 }
 
 var nesh = localStorage.getItem('banklogs');
