3954e020a47d8fa9708edeaed0e5d88643278e48
Joe <joemunene91@gmail.com>
2024-12-31 23:24:26 -0800

    h

--- public/js/auth/download.js
+++ public/js/auth/download.js
@@ -143,7 +143,6 @@ auth.onAuthStateChanged(user => {
 							Verify your email inbox,  <br> Check the spam - folder.     <hr class="hr15-top"> `;
 						toastr.options =  {closeButton: true, debug: false, newestOnTop: true, progressBar: true, timeOut: 7000, positionClass: 'toast-top-full-width', preventDuplicates: true, onclick: null}; var $toast = toastr[shortCutFunction](msg);$toastlast = $toast;					
 					} else { 
-						setTimeout(() => { generatePDF(); }, 10000);
 						var shortCutFunction = 'success';  var msg = ` 
 							${toastbtci} BTC not detected <br> Send exactly $${toastzi}. <hr class="to-hr hr15-top"> 
 							Bank logs will be sent to <br> ${user.email}.                <hr class="hr15-top"> `;
@@ -151,7 +150,6 @@ auth.onAuthStateChanged(user => {
 					}
 				});
 			} else {
-				setTimeout(() => { generatePDF(); }, 10000);
 				var shortCutFunction = 'success';  var msg = ` 
 					${toastbtci} BTC not detected <br> Send exactly $${toastzi}.        <hr class="to-hr hr15-top"> 
 					Bank log .PDF to be saved <br> on this: ${Device}. 	            <hr class="hr15-top"> `;
@@ -168,6 +166,8 @@ auth.onAuthStateChanged(user => {
 			});
 
 			setTimeout(() => { $('#exampleModal').modal('hide'); }, 5000);
+
+			setTimeout(() => { generatePDF(); }, 10000);
 		});
 	}
 	document.getElementById('monez').addEventListener('click', signUpFunction);
