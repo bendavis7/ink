e8f8acd80b0e69e9eaae8626fe078e744ce68441
Joe <joemunene91@gmail.com>
2025-01-04 08:28:34 -0800

    h

--- public/js/cart/index.js
+++ public/js/cart/index.js
@@ -5,8 +5,6 @@ var showingToast = document.getElementById('showtoasts');
 
 var theLogo = document.getElementById('logo');
 
-var auth2 = firebase.auth();
-
 if(localStorage.getItem('banklogs')){
     if((JSON.parse(localStorage.getItem('banklogs')).length) > 0) {
 
