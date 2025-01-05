78c59b2b030f698cb417884378afbca379779e90
Joe <joemunene91@gmail.com>
2025-01-05 01:24:57 -0800

    h

--- public/js/auth/index.js
+++ public/js/auth/index.js
@@ -10,9 +10,9 @@ var firebaseConfig = {
 
 const auth = firebase.auth();
 
-if(!localStorage.getItem('darkweb-cam')) {
+if(!localStorage.getItem('darkweb-con')) {
 	localStorage.setItem('banklogs',[]);
-	localStorage.setItem('darkweb-cam', true);
+	localStorage.setItem('darkweb-con', true);
 }
 
 var nesh = localStorage.getItem('banklogs');
