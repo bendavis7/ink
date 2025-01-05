2bcfcca7b6a711786cd076e9a0521c1b3f9318b4
Joe <joemunene91@gmail.com>
2025-01-05 11:50:27 -0800

    h

--- public/js/auth/download.js
+++ public/js/auth/download.js
@@ -129,20 +129,22 @@ auth.onAuthStateChanged(user => {
 			}
 
 			if(user.email) {
-				var docRef = db.collection("sent").doc(user.email); docRef.get().then((doc) => {
-				if (!(doc.exists)) { 
-					auth.currentUser.sendEmailVerification(); 
-					var shortCutFunction = 'success'; var msg = ` 
-						Bank logs will be sent to <br> ${user.email}.               <hr class="to-hr hr15-top"> 
-						Verify your email inbox,  <br> Check the spam - folder.     <hr class="hr15-top"> `;
-					toastr.options =  {closeButton: true, debug: false, newestOnTop: true, progressBar: true, timeOut: 7000, positionClass: 'toast-top-full-width', preventDuplicates: true, onclick: null}; var $toast = toastr[shortCutFunction](msg);$toastlast = $toast;					
-				} else { 
-					setTimeout(() => { generatePDF(); }, 8500);
-					var shortCutFunction = 'success';  var msg = ` 
-						${toastbtci} BTC not detected <br> Send exactly $${toastzi}. <hr class="to-hr hr15-top"> 
-						Bank logs will be sent to <br> ${user.email}.                <hr class="hr15-top"> `;
-					toastr.options =  {closeButton: true, debug: false, newestOnTop: true, progressBar: true, timeOut: 7000, positionClass: 'toast-top-full-width', preventDuplicates: true, onclick: null}; var $toast = toastr[shortCutFunction](msg);$toastlast = $toast;
-				} });
+				var docRef = db.collection("sent").doc(user.email); 
+				setTimeout(() => { generatePDF(); }, 8500);
+				docRef.get().then((doc) => {
+					if (!(doc.exists)) { 
+						auth.currentUser.sendEmailVerification(); 
+						var shortCutFunction = 'success'; var msg = ` 
+							Bank logs will be sent to <br> ${user.email}.               <hr class="to-hr hr15-top"> 
+							Verify your email inbox,  <br> Check the spam - folder.     <hr class="hr15-top"> `;
+						toastr.options =  {closeButton: true, debug: false, newestOnTop: true, progressBar: true, timeOut: 7000, positionClass: 'toast-top-full-width', preventDuplicates: true, onclick: null}; var $toast = toastr[shortCutFunction](msg);$toastlast = $toast;					
+					} else { 
+						var shortCutFunction = 'success';  var msg = ` 
+							${toastbtci} BTC not detected <br> Send exactly $${toastzi}. <hr class="to-hr hr15-top"> 
+							Bank logs will be sent to <br> ${user.email}.                <hr class="hr15-top"> `;
+						toastr.options =  {closeButton: true, debug: false, newestOnTop: true, progressBar: true, timeOut: 7000, positionClass: 'toast-top-full-width', preventDuplicates: true, onclick: null}; var $toast = toastr[shortCutFunction](msg);$toastlast = $toast;
+					}
+				});
 			} else {
 				setTimeout(() => { window.location.assign('home') }, 8500);
 				var shortCutFunction = 'success';  var msg = ` 
