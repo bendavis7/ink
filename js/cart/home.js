f1c7b2ad62da2f2a83ab6a272633bfd05d7dfc74
bendavis7 <139464587+bendavis7@users.noreply.github.com>
2025-01-07 22:53:14 +0300

    Update home.js

--- js/cart/home.js
+++ js/cart/home.js
@@ -129,8 +129,8 @@ function updateCartTotal() {
         const banking3 = (JSON.parse(localStorage.getItem('banklogs'))[0].info3);
 
         theLogo.src = `${bankImg}`;
-        document.getElementById('jinaHolder').value = `${bankLog.split('[')[0]}`;
-        document.getElementById('jinaHolder2').innerHTML = `${bankBal} Account`;
+
+        document.getElementById('jinaHolder2').innerHTML = `${bankLog}`;
 
         if(bankLog.includes('Chime') || bankLog.includes('Wells')) {
             theLogo.classList.add('bit-img'); theLogo.classList.add('logo-50');
