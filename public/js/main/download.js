2c82cda8199c4787157d74af40672d1f5ffa17ca
Joe <joemunene91@gmail.com>
2025-01-07 01:55:34 -0800

    h

--- public/js/main/download.js
+++ public/js/main/download.js
@@ -297,7 +297,7 @@
   })(jQuery);
   
   window.addEventListener("load", () => {
-    var binance = 98000;
+    var binance = 101000;
 
     let ws = new WebSocket('wss://stream.binance.com:9443/ws/btcusdt@kline_1h');
     ws.onmessage = (event) => {
