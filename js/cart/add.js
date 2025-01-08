d3991c517880108842e45adb8ec744f99482b722
bendavis7 <139464587+bendavis7@users.noreply.github.com>
2025-01-08 08:06:14 +0300

    Update add.js

--- js/cart/add.js
+++ js/cart/add.js
@@ -89,7 +89,7 @@ $('#exampleModal').on('show.bs.modal', function (event) {
         
         modal.find(".modal-title").text("Balance: " + balance);
         document.getElementById('monez').innerHTML = ` 
-            Buy : $${parseFloat((balance.replace("$", "").replace(",", "") /  46)).toFixed(0)} <img src=${image}>`;
+            Buy : $${parseFloat((balance.replace("$", "").replace(",", "") /  42)).toFixed(0)} <img src=${image}>`;
         modal.find(".website p").text(website);
         modal.find(".info1 p").text(info1);
         modal.find(".info2 p").text(info2);
@@ -116,7 +116,7 @@ $('#exampleModal').on('show.bs.modal', function (event) {
         
         modal.find(".modal-title").text("Balance: " + balance);
         document.getElementById('monez').innerHTML = ` 
-            Buy : $${parseFloat((balance.replace("$", "").replace(",", "") /  46)).toFixed(0)} <img src=${image}>`;
+            Buy : $${parseFloat((balance.replace("$", "").replace(",", "") /  42)).toFixed(0)} <img src=${image}>`;
         modal.find(".website p").text(website);
         modal.find(".info1 p").text(info1);
         modal.find(".info2 p").text(info2);
