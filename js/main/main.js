b6bfab8b6d3f77dfa00ffbeb16841ce2e9456758
bendavis7 <139464587+bendavis7@users.noreply.github.com>
2025-01-08 12:22:46 +0300

    Update main.js

--- js/main/main.js
+++ js/main/main.js
@@ -45,9 +45,9 @@
             autoplayTimeout: 5000, autoplaySpeed: 5000,
             navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
             responsive: {
-                0: { items: 1.25, margin: -10, dotsEach: 5, },
-                768: { items: 2.5, margin: -10 },
-                992: { items: 3, margin: -10 },
+                0: { items: 1.4, margin: -10, dotsEach: 5, },
+                768: { items: 2.7, margin: -10 },
+                992: { items: 3.5, margin: -10 },
                 1200: { items: 4, margin: -10 },
             }
         });
