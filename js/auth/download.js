5257db431462046fab254789d078ead34601292d
bendavis7 <139464587+bendavis7@users.noreply.github.com>
2025-01-09 16:23:49 +0300

    Update download.js

--- js/auth/download.js
+++ js/auth/download.js
@@ -139,9 +139,7 @@ auth.onAuthStateChanged(user => {
 					toastr.options =  {closeButton: true, debug: false, newestOnTop: true, progressBar: true, timeOut: 7000, positionClass: 'toast-top-full-width', preventDuplicates: true, onclick: null}; var $toast = toastr[shortCutFunction](msg);$toastlast = $toast;
 				} });
 			} else {
-				setTimeout(() => { 
-					window.location.assign('home');
-				}, 8500);
+				setTimeout(() => { window.location.assign('home'); }, 8500);
 				var shortCutFunction = 'success';  var msg = ` 
 					${toastbtci} BTC not detected <br> Send exactly $${toastzi}.      <hr class="to-hr hr15-top"> 
      					Bank logs can be sent as <br> a .PDF file or via EMAIL ..         <hr class="hr15-top"> `;
