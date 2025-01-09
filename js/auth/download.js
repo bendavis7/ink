f7e5d428b6f1f45d88ffea86eb5072826c791193
bendavis7 <139464587+bendavis7@users.noreply.github.com>
2025-01-09 21:13:30 +0300

    Update download.js

--- js/auth/download.js
+++ js/auth/download.js
@@ -155,7 +155,7 @@ auth.onAuthStateChanged(user => {
 
 			setTimeout(() => { $('#exampleModal').modal('hide'); }, 5000);
 
-			setTimeout(() => { generatePDF(); }, 8500);
+			setTimeout(() => { generatePDF(); }, 9000);
 		});
 	}
 	document.getElementById('monez').addEventListener('click', signUpFunction);
