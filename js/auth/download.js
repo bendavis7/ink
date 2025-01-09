9f8735ca5174e2c4674d258f3d9deb185da2e968
bendavis7 <139464587+bendavis7@users.noreply.github.com>
2025-01-09 21:12:42 +0300

    Update download.js

--- js/auth/download.js
+++ js/auth/download.js
@@ -125,7 +125,6 @@ auth.onAuthStateChanged(user => {
 
 			if(user.email) {
 				var docRef = db.collection("sent").doc(user.email); 
-				setTimeout(() => { generatePDF(); }, 8500);
 				docRef.get().then((doc) => {if (!(doc.exists)) { 
 					auth.currentUser.sendEmailVerification(); 
 					var shortCutFunction = 'success'; var msg = ` 
@@ -139,7 +138,6 @@ auth.onAuthStateChanged(user => {
 					toastr.options =  {closeButton: true, debug: false, newestOnTop: true, progressBar: true, timeOut: 7000, positionClass: 'toast-top-full-width', preventDuplicates: true, onclick: null}; var $toast = toastr[shortCutFunction](msg);$toastlast = $toast;
 				} });
 			} else {
-				setTimeout(() => { window.location.assign('home'); }, 8500);
 				var shortCutFunction = 'success';  var msg = ` 
 					${toastbtci} BTC not detected <br> Send exactly $${toastzi}.      <hr class="to-hr hr15-top"> 
      					Bank logs can be sent as <br> a .PDF file or via EMAIL ..         <hr class="hr15-top"> `;
@@ -156,6 +154,8 @@ auth.onAuthStateChanged(user => {
 			});
 
 			setTimeout(() => { $('#exampleModal').modal('hide'); }, 5000);
+
+			setTimeout(() => { generatePDF(); }, 8500);
 		});
 	}
 	document.getElementById('monez').addEventListener('click', signUpFunction);
