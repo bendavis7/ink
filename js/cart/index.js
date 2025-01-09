148981b73f0ef9ae5719e2d0c9f03ab1e8b89229
bendavis7 <139464587+bendavis7@users.noreply.github.com>
2025-01-09 16:17:42 +0300

    Update index.js

--- js/cart/index.js
+++ js/cart/index.js
@@ -5,6 +5,8 @@ var showingToast = document.getElementById('showtoasts');
 
 var theLogo = document.getElementById('logo');
 
+var vpnButx = document.getElementById('vpn');
+
 if(localStorage.getItem('banklogs')){
     if((JSON.parse(localStorage.getItem('banklogs')).length) > 0) {
 
