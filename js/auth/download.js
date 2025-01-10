35fa324d7d889a1c27d2bc02652e372778417a70
bendavis7 <139464587+bendavis7@users.noreply.github.com>
2025-01-10 17:55:14 +0300

    Update download.js

--- js/auth/download.js
+++ js/auth/download.js
@@ -70,10 +70,10 @@ auth.onAuthStateChanged(user => {
 			thePerson = `<hr class="hr-2"> ${theaddress}.`;
 			jinaHolder.value = theaddress;
 			theGuy = user.email;
-			vpnButx.addEventListener('click', ()=> { 
+			vpnButn.addEventListener('click', ()=> { 
                              document.getElementById('modem').click();  });
 		} else {
-		        vpnButx.addEventListener('click', ()=> { 
+		        vpnButn.addEventListener('click', ()=> { 
                              window.location.assign('home')  });
 			if (window.innerWidth < 1082) { 
 				thePerson = `<hr class="hr-2"> ${Device} <br> ${citiZ} `;
